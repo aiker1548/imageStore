719cb718e97fa5eb86e2bcdf98f9ca8d7df51d47
aiker <test@example.com>
2024-11-04 03:27:40 +0300

    fix:change DATABESA_URL

--- backend/db/create_db.py
+++ backend/db/create_db.py
@@ -5,8 +5,8 @@ import os
 
 load_dotenv()
 
+DATABASE_URL = f"mysql+mysqlconnector://username:password@{os.getenv('DB_URL')}/image_db"
 
-DATABASE_URL = f"mysql+mysqlconnector://username:password@{os.getenv("DB_URL")}/image_db"
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
